25ae8e68f25a9438c135923b8c5fb5024ff7da87
Tylor Schafer <tylorschafer@gmail.com>
2019-12-02 13:54:58 -0700

    Initial commit

--- routes/api/v1/papers.js
+++ routes/api/v1/papers.js
@@ -1,19 +1,18 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../../../knexfile')[environment];
-const database = require('knex')(configuration);
+// const environment = process.env.NODE_ENV || 'development';
+// const configuration = require('../../../knexfile')[environment];
+// const database = require('knex')(configuration);
 
+// router.get('/', (request, response) => {
+//   database('papers').select()
+//     .then((papers) => {
+//       response.status(200).json(papers);
+//     })
+//     .catch((error) => {
+//       response.status(500).json({ error });
+//     });
+// });
 
-router.get('/', (request, response) => {
-  database('papers').select()
-    .then((papers) => {
-      response.status(200).json(papers);
-    })
-    .catch((error) => {
-      response.status(500).json({ error });
-    });
-});
-
-module.exports = router;
+// module.exports = router;
