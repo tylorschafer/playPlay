0c4efe509cfd2340b7d03c800cc4a090878df70a
Tylor Schafer <tylorschafer@gmail.com>
2019-12-05 17:45:49 -0700

    Fix spacing in post playlist

--- app/routes/api/v1/playlists.js
+++ app/routes/api/v1/playlists.js
@@ -5,12 +5,12 @@ const configuration = require('../../../../knexfile')[environment]
 const database = require('knex')(configuration)
 
 router.post('/', async (request, response) => {
-    const title = request.body.title 
-    database('playlists')
-     .returning(['id', 'title', 'created_at', 'updated_at'])
-     .insert({ title: title })
-     .then(result => { response.status(201).json(result)})
-     .catch(error => { response.status(400).json({ error }) })
+  const title = request.body.title
+  database('playlists')
+    .returning(['id', 'title', 'created_at', 'updated_at'])
+    .insert({ title: title })
+    .then(result => { response.status(201).json(result) })
+    .catch(error => { response.status(400).json({ error }) })
 })
 
-module.exports = router 
+module.exports = router
