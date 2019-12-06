0e42e67987ad5de6f195d114dc8e5c268642fa65
Tylor Schafer <tylorschafer@gmail.com>
2019-12-05 19:57:45 -0700

    Add put playlist endpoint

--- app/routes/api/v1/playlists.js
+++ app/routes/api/v1/playlists.js
@@ -13,4 +13,16 @@ router.post('/', async (request, response) => {
     .catch(error => { response.status(400).json({ error }) })
 })
 
+router.put('/:id', async (request, response) => {
+  const newTitle = request.body.title
+  const id = request.params.id
+
+  database('playlists')
+    .returning(['id', 'title', 'created_at', 'updated_at'])
+    .where('id', id)
+    .update({ title: newTitle })
+    .then(result => { response.status(200).json(result) })
+    .catch(error => { response.status(404).json(error) })
+})
+
 module.exports = router
