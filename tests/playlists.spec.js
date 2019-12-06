c57caf42a61d8e045294d15e2a04dc72dc56594a
Tylor Schafer <tylorschafer@gmail.com>
2019-12-05 19:38:46 -0700

    fix indentation in playlists spec

--- tests/playlists.spec.js
+++ tests/playlists.spec.js
@@ -7,20 +7,20 @@ const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
 describe('Test Playlist Endpoints', () => {
-    beforeEach(async () => {
-        await database.raw('truncate table playlists cascade')
-    })
-    afterEach(() => {
-        database.raw('truncate table playlists cascade')
-    })
+  beforeEach(async () => {
+    await database.raw('truncate table playlists cascade')
+  })
+  afterEach(() => {
+    database.raw('truncate table playlists cascade')
+  })
 
-    describe('Post Playlist', () => {
-        it('Creates a new playlist', async () => {
-            const res = await request(app)
-            .post('/api/v1/playlists')
-            .send({ title: 'Working Out'})
-        expect(res.body[0].id).toBe(1)
-        expect(res.body[0].title).toBe('Working Out')
-        })
+  describe('Post Playlist', () => {
+    it('Creates a new playlist', async () => {
+      const res = await request(app)
+        .post('/api/v1/playlists')
+        .send({ title: 'Working Out' })
+      expect(res.body[0].id).toBe(1)
+      expect(res.body[0].title).toBe('Working Out')
     })
+  })
 })
