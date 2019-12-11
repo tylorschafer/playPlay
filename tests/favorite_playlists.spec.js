84dbbbf8b3a8f26f0905c5592cd021639870cf41
Tylor Schafer <tylorschafer@gmail.com>
2019-12-10 19:04:24 -0700

    Add delete favorite from playlist testing

--- tests/favorite_playlists.spec.js
+++ tests/favorite_playlists.spec.js
@@ -27,4 +27,24 @@ describe('Test Playlist Endpoints', () => {
       expect(res.body.success).toBe(`${favorite.title} has been added to ${playlist.title}!`)
     })
   })
+
+  describe('Delete Favorite of Playlist', () => {
+    it('Deletes a favorite from the joins table', async () => {
+      const playlist = await database('playlists')
+        .insert({ title: 'Driving' })
+        .returning(['id'])
+        .then(result => result[0])
+      const favorite = await database('favorites')
+        .insert({ title: 'Tiny Dancer', artistName: 'Elton John', rating: 99, genre: 'Classic Pop' })
+        .returning(['id'])
+        .then(result => result[0])
+      const playFav = await database('playlist_favorites')
+        .insert({ favorite_id: favorite.id, playlist_id: playlist.id })
+        .then(result => result[0])   
+      const res = await request(app)
+        .delete(`/api/v1/playlists/${playlist.id}/favorites/${favorite.id}`)
+
+      expect(res.statusCode).toBe(201)
+    })
+  })
 })
