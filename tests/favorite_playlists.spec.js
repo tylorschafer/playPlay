3f40cddf5d44c24839ec9f3a80dccf27445f7cc0
Tylor Schafer <tylorschafer@gmail.com>
2019-12-10 18:51:49 -0700

    Fix indentation in spec file

--- tests/favorite_playlists.spec.js
+++ tests/favorite_playlists.spec.js
@@ -7,24 +7,24 @@ const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
 describe('Test Playlist Endpoints', () => {
-    beforeEach(async () => {
-        await database.raw('truncate table playlist_favorites cascade')
-    })
-    afterEach(() => {
-        database.raw('truncate table playlist_favorites cascade')
-    })
+  beforeEach(async () => {
+    await database.raw('truncate table playlist_favorites cascade')
+  })
+  afterEach(() => {
+    database.raw('truncate table playlist_favorites cascade')
+  })
 
-    describe('Post Favorite to Playlist', () => {
-        it('Adds a favorite entry into a playlist', async () => {
-            const playlist = await database('playlists').insert({ title: 'Driving' }).returning(['id', 'title']).then(result => result[0])
-            const favorite = await database('favorites').insert({ title: 'Tiny Dancer', artistName: 'Elton John', rating: 99, genre: 'Classic Pop'})
-                                                        .returning(['id', 'title'])
-                                                        .then(result => result[0])
-  
-            const res = await request(app)
-                .post(`/api/v1/playlists/${playlist.id}/favorites/${favorite.id}`)  
+  describe('Post Favorite to Playlist', () => {
+    it('Adds a favorite entry into a playlist', async () => {
+      const playlist = await database('playlists').insert({ title: 'Driving' }).returning(['id', 'title']).then(result => result[0])
+      const favorite = await database('favorites').insert({ title: 'Tiny Dancer', artistName: 'Elton John', rating: 99, genre: 'Classic Pop' })
+        .returning(['id', 'title'])
+        .then(result => result[0])
 
-            expect(res.body.success).toBe(`${favorite.title} has been added to ${playlist.title}!`)
-        }) 
+      const res = await request(app)
+        .post(`/api/v1/playlists/${playlist.id}/favorites/${favorite.id}`)
+
+      expect(res.body.success).toBe(`${favorite.title} has been added to ${playlist.title}!`)
     })
+  })
 })
